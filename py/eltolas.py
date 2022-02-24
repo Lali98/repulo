272474a12c09afe5167756bb111e89b51068b595
Lali98 <89679432+Lali98@users.noreply.github.com>
2022-02-24 21:21:50 +0100

    Nem teljesen kész

---
+++ py/eltolas.py
@@ -0,0 +1,6 @@
+from xyw import *
+from fibonacci_50 import *
+from after_z import *
+
+def elt():
+    return xyw() + after_z() + fibonacci()
--- py/main.py
+++ py/main.py
@@ -1,5 +1,30 @@
-from xyw import *
-from fibonacci_50 import *
-from after_z import *
+from eltolas import *
+import string
 
-print(xyw() + after_z() + fibonacci())
+betuk = string.ascii_letters
+print(betuk)
+
+e_m = []
+
+with open("caesar.txt", "r") as file:
+    for sor in file:
+        e_m.append(sor.strip())
+
+
+print()
+key = elt()
+
+d_m = ""
+
+for sor in e_m:
+    for c in sor:
+        if c in betuk:
+            hely = betuk.find(c)
+            uj_hely = (hely - key) % 26
+            uj_kar = betuk[uj_hely]
+            d_m += uj_kar
+        else:
+            d_m += c
+
+    print(f"Eredmény: {d_m}")
+    d_m = ""
